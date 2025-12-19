840c382d795decdd42efc3d4ecc01cc19c7aa788
Aalim Jannaty <aalim.jannaty@gmail.com>
2025-12-19 17:53:49 -0300

    added pytest for wallet deposit

---
+++ wallets/tests/test_deposit.py
@@ -0,0 +1,63 @@
+from django.test import TestCase
+import pytest
+from decimal import Decimal
+from django.urls import reverse
+from rest_framework.test import APIClient
+from wallets.models import Wallet, Transaction
+
+from users.models import User
+
+@pytest.mark.django_db
+class TestDepositAPI:
+
+    def setup_method(self):
+        self.client = APIClient()
+
+        self.user = User.objects.create_user(
+            email = "test@example.com",
+            password = "testpass123",
+            name = "Test User"
+        )
+
+        self.wallet = Wallet.objects.get(user=self.user)
+
+        self.client.force_authenticate(user=self.user)
+        self.url = "/api/v1/wallets/deposit"
+
+    def test_deposit_success(self):
+        response = self.client.post(
+            self.url,
+            {"amount": "100.00"},
+            format="json"
+        )
+
+        assert response.status_code == 200
+
+        self.wallet.refresh_from_db()
+
+        assert self.wallet.balance == Decimal("100.00")
+
+        transaction = Transaction.objects.get(wallet=self.wallet)
+        assert transaction.amount == Decimal("100.00")
+        assert transaction.type == Transaction.Type.DEPOSIT
+
+    def test_depsosit_invalid_amount(self):
+        response = self.client.post(
+            self.url,
+            {"amount":"-10"},
+            format="json"
+        )
+
+        assert response.status_code == 400
+
+    def test_deposit_requires_authentication(self):
+        client = APIClient()
+
+        response = client.post(
+            self.url,
+            {"amount": "50"},
+            format="json"
+        )
+
+        assert response.status_code == 401
+
